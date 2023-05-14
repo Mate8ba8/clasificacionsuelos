ccc66447c21c95448e4fa192d3bb8a94258a4296
Mateo Bohorquez <132773064+Mate8ba8@users.noreply.github.com>
2023-05-14 15:39:18 -0500

    Create Valoresdeentrada.py

---
+++ functions/Valoresdeentrada.py
@@ -0,0 +1,11 @@
+# Por favor diligencia la cantidad en gramos del suelo retenido 
+Cantidad_retenida_tam4 = 0
+Cantidad_retenida_tam10 = 0
+Cantidad_retenida_tam20 = 25.7
+Cantidad_retenida_tam40 = 40.4
+Cantidad_retenida_tam100 = 18.1
+Cantidad_retenida_tam200 = 47.4 
+Cantidad_retenida_Tara = 200
+
+Limite_liquido = 80
+Indice_plasticidad = 20
